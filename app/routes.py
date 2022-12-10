550089cc2079171ac0ec53c80eec1ed991d715b5
Ashley Fightmaster <115577226+AshleyFightmaster@users.noreply.github.com>
2022-12-09 19:06:13 -0700

    "update app further"

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/pokemon', methods=['GET', 'POST'])
 @login_required
-def pokemon():
+def pokemon(user_id):
     form = Pokesearchform()
     pokemon_info={}
     if request.method == 'POST':
@@ -23,17 +23,15 @@ def pokemon():
             search = form.search.data
             url = f'https://pokeapi.co/api/v2/pokemon/{search}'
             response = requests.get(url)
-            
             pokemon_info ={
-                        'name' : response.json()['forms'][0]['name'],
-                        'ability_name' : response.json()['abilities'][0]['ability']['name'],
-                        "base_experience": response.json()['base_experience'],
-                        "sprite" : response.json()['sprites']['front_default'],
-                        "attack_base_stat" : response.json()['stats'][1]['base_stat'],
-                        "hp_base_stat": response.json()['stats'][0]['base_stat'],
-                        "defense_base_stat" : response.json()['stats'][2]['base_stat']
-                }
-            
+                'name' : response.json()['forms'][0]['name'],
+                'ability_name' : response.json()['abilities'][0]['ability']['name'],
+                "base_experience": response.json()['base_experience'],
+                "sprite" : response.json()['sprites']['front_default'],
+                "attack_base_stat" : response.json()['stats'][1]['base_stat'],
+                "hp_base_stat": response.json()['stats'][0]['base_stat'],
+                "defense_base_stat" : response.json()['stats'][2]['base_stat']
+            } 
             name = pokemon_info['name']
             ability_name = pokemon_info['ability_name']
             base_experience = pokemon_info['base_experience']
@@ -44,27 +42,23 @@ def pokemon():
             
             pokemon = Pokemon(name, base_stat_hp, base_stat_defense, base_stat_attack, sprite, ability_name, base_experience)
             poke = Pokemon.query.get(name)
-            team = Team.query.get(user_id, pokemon_name)
+            team = Team.query.get(user_id)
             if poke:
                 flash("That pokemon has already been caught!")
             elif current_user.id == team.user_id:
                 if team.pokemon_name == 5:
                     flash("Your pokemon deck is full. Delete another pokemon to catch more.")
+                    return redirect(url_for('pokemon_team.view_team'))
             elif poke < 1:
-                flash(f'Successfully caught {pokemon.name.title()}!', 'succuss')
+                flash(f'Successfully caught {poke.title()}!', 'succuss')
                 pokemon.save_to_db()
+                return redirect(url_for('app.pokemon'))
             else:
                 flash('Check spelling.', 'warning')
-        
-            # response.raise_for_status()
-            # if response.status_code != 204:
-            #     return response.json()
-            # elif response.status_code == 200
-            # return redirect(url_for('pokemon2'))
+
+            return render_template('pokemon.html')
+            
 
     return render_template('pokemon.html', form=form, pokemon_info=pokemon_info)
 
 
-
-    
-    
